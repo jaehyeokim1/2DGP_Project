97d07ee407ab3bc9464c2eb85ee919c756eb8de6
jaehyeokim0921 <qoxmaos921@naver.com>
2024-11-30 00:01:03 +0900

    이전 경기의 공 뱡향 속도 영향받는 문제점 해결

--- point.py
+++ point.py
@@ -21,12 +21,23 @@ class Point:
             play_mode.ball.x, play_mode.ball.y = 150, 550
             print("R_bottom")
 
+        # 공의 속도 초기화 (이전 방향 영향을 받지 않도록)
+        play_mode.ball.tx = 0
+        play_mode.ball.ty = 6  # 초기 속도 값 지정
+        play_mode.ball.speed_y = 6  # 수직 속도 초기화
+
+        # 공의 방향 설정 (왼쪽 또는 오른쪽으로 시작)
+        if is_left:
+            play_mode.ball.tx = -5  # 왼쪽 방향으로 이동
+        else:
+            play_mode.ball.tx = 5  # 오른쪽 방향으로 이동
+
+        # 피카츄 위치 초기화
         play_mode.pikachu2.x = 650
         play_mode.pikachu.x = 150
         play_mode.pikachu.jump = 0
         play_mode.pikachu2.jump = 0
-        play_mode.ball.to_x = 0
-        play_mode.ball.to_y = 0
+
         delay(1)
 
     def update(self):
